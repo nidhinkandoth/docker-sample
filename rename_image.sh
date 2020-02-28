8a7a55a6fee8f2957bfabe0d4358617fcf3ed48c
Nidhin K <nidhinkandoth2016@gmail.com>
2020-02-28 11:22:54 +0530

    Update rename_image.sh

--- rename_image.sh
+++ rename_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Copying ${OUTPUT_THUMBS_FILE_NAME} to S3 at ${OUTPUT_S3_PATH}/${OUTPUT_THUMBS_FILE_NAME} ..."
-aws s3 cp s3://nidhin/fargate-video-conversion/yourstory_track.png s3://nidhin/fargate-video-conversion/test.png --region ap-south-1
+echo "executing task"
+aws s3 cp s3://entri-app-videos/fargate-video-conversion/yourstory_track.png s3://entri-app-videos/fargate-video-conversion/test.png --region ap-south-1
